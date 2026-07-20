652178d8ede60dcbc81d093434a6cbdddf6c25ab
Alessio Bifulco <alessio.bifulco@studio.unibo.it>
2026-07-20 15:29:26 +0200

    feat(domain): add V2d normalized & V2d random

--- src/main/scala/domain/V2d.scala
+++ src/main/scala/domain/V2d.scala
@@ -5,7 +5,14 @@ case class V2d(x: Double, y: Double)
 object V2d:
   val zero: V2d = V2d(0, 0)
 
+  def random(): V2d =
+    val angle = math.random() * 2 * math.Pi
+    V2d(math.cos(angle), math.sin(angle))
+
   extension (v: V2d)
     def +(other: V2d): V2d = V2d(v.x + other.x, v.y + other.y)
     def *(factor: Double): V2d = V2d(v.x * factor, v.y * factor)
     def length: Double = math.sqrt(v.x * v.x + v.y * v.y)
+    def normalized: V2d =
+      val l = v.length
+      if l == 0 then V2d.zero else V2d(v.x / l, v.y / l)
