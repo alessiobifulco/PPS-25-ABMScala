7f29165882b2a5984fde043e14674970bd79a2da
Samuele Ferri <samuele.ferri3@studio.unibo.it>
2026-07-21 15:40:35 +0200

    refactor(domain): add require in Bounds

--- src/main/scala/domain/Bounds.scala
+++ src/main/scala/domain/Bounds.scala
@@ -2,15 +2,18 @@ package domain
 
 final case class Bounds(width: Double, height: Double):
 
-  def contains(position: P2d): Boolean =
-    position.x >= 0 && position.x <= width && position.y >= 0 && position.y <= height
+  require(width > 0, "Width must be positive")
+  require(height > 0, "Height must be positive")
 
-  def clamp(position: P2d): P2d =
-    P2d(x = position.x.max(0).min(width), y = position.y.max(0).min(height))
+  def contains(position: P2d): Boolean = position.x >= 0 && position.x <= width && position.y >= 0 &&
+    position.y <= height
+
+  def clamp(position: P2d): P2d = P2d(x = position.x.max(0).min(width), y = position.y.max(0).min(height))
 
   def bounce(position: P2d, velocity: V2d): (P2d, V2d) =
     val shouldBounceX = (position.x <= 0 && velocity.x < 0) || (position.x >= width && velocity.x > 0)
     val shouldBounceY = (position.y <= 0 && velocity.y < 0) || (position.y >= height && velocity.y > 0)
     val correctedPosition = clamp(position)
-    val correctedVelocity = V2d(x = if shouldBounceX then -velocity.x else velocity.x, y = if shouldBounceY then -velocity.y else velocity.y)
+    val correctedVelocity =
+      V2d(x = if shouldBounceX then -velocity.x else velocity.x, y = if shouldBounceY then -velocity.y else velocity.y)
     (correctedPosition, correctedVelocity)
