b77c0d0c3abce470e4f975620d6681c11d84db4e
Samuele Ferri <samuele.ferri3@studio.unibo.it>
2026-07-21 18:46:42 +0200

    refactor(domain): introduce Space and RectangularSpace

--- src/main/scala/domain/Bounds.scala
+++ src/main/scala/domain/Bounds.scala
@@ -1,19 +1,14 @@
 package domain
 
-final case class Bounds(width: Double, height: Double):
+final case class Bounds(width: Double, height: Double) extends Space:
 
-  require(width > 0, "Width must be positive")
-  require(height > 0, "Height must be positive")
+  private val rectangularSpace = RectangularSpace(width, height)
 
-  def contains(position: P2d): Boolean = position.x >= 0 && position.x <= width && position.y >= 0 &&
-    position.y <= height
+  override def contains(position: P2d): Boolean =
+    rectangularSpace.contains(position)
 
-  def clamp(position: P2d): P2d = P2d(x = position.x.max(0).min(width), y = position.y.max(0).min(height))
+  override def clamp(position: P2d): P2d =
+    rectangularSpace.clamp(position)
 
-  def bounce(position: P2d, velocity: V2d): (P2d, V2d) =
-    val shouldBounceX = (position.x <= 0 && velocity.x < 0) || (position.x >= width && velocity.x > 0)
-    val shouldBounceY = (position.y <= 0 && velocity.y < 0) || (position.y >= height && velocity.y > 0)
-    val correctedPosition = clamp(position)
-    val correctedVelocity =
-      V2d(x = if shouldBounceX then -velocity.x else velocity.x, y = if shouldBounceY then -velocity.y else velocity.y)
-    (correctedPosition, correctedVelocity)
+  override def bounce(position: P2d, velocity: V2d): (P2d, V2d) =
+    rectangularSpace.bounce(position, velocity)
