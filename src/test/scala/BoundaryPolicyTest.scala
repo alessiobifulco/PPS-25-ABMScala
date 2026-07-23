7beedab4d82e835f87a1ba2a97c8d4632ce701e0
Samuele Ferri <samuele.ferri3@studio.unibo.it>
2026-07-23 10:53:11 +0200

    test: add boundary policy specifications

--- src/test/scala/BoundaryPolicyTest.scala
+++ src/test/scala/BoundaryPolicyTest.scala
@@ -1,4 +1,4 @@
-import domain.{BouncePolicy, P2d, RectangularSpace, V2d}
+import domain.{BouncePolicy, P2d, RectangularSpace, StopPolicy, V2d, WrapPolicy}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
@@ -15,3 +15,18 @@ class BoundaryPolicyTest extends AnyFlatSpec with Matchers:
     val position = P2d(20.0, 20.0)
     val velocity = V2d(2.0, 1.0)
     BouncePolicy(position, velocity, space) shouldBe (position, velocity)
+
+  it should "wrap positions to the opposite side" in:
+    val position = P2d(-10.0, 60.0)
+    val velocity = V2d(-2.0, 1.0)
+    WrapPolicy(position, velocity, space) shouldBe (P2d(90.0, 10.0), velocity)
+
+  it should "stop an agent outside the space" in:
+    val position = P2d(-10.0, 20.0)
+    val velocity = V2d(-2.0, 1.0)
+    StopPolicy(position, velocity, space) shouldBe (P2d(0.0, 20.0), V2d.zero)
+
+  it should "preserve an inward velocity at the boundary" in:
+    val position = P2d(0.0, 20.0)
+    val velocity = V2d(2.0, 1.0)
+    StopPolicy(position, velocity, space) shouldBe (position, velocity)
