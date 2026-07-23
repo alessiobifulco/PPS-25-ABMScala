f34030e5555657d5d28d508c327ce79b2293d392
Samuele Ferri <samuele.ferri3@studio.unibo.it>
2026-07-23 15:30:55 +0200

    refactor(domain): move wrap to a separate Toroidal capability

--- src/main/scala/domain/BoundaryPolicy.scala
+++ src/main/scala/domain/BoundaryPolicy.scala
@@ -1,26 +1,20 @@
 package domain
 
 trait BoundaryPolicy:
-
   def apply(candidatePosition: P2d, currentVelocity: V2d, space: Space): (P2d, V2d)
 
 case object BouncePolicy extends BoundaryPolicy:
-
-  override def apply(candidatePosition: P2d, currentVelocity: V2d, space: Space): (P2d, V2d) = space
-    .bounce(candidatePosition, currentVelocity)
-
-case object WrapPolicy extends BoundaryPolicy:
-
-  override def apply(candidatePosition: P2d, currentVelocity: V2d, space: Space): (P2d, V2d) =
-    (space.wrap(candidatePosition), currentVelocity)
+  override def apply(position: P2d, velocity: V2d, space: Space): (P2d, V2d) = space.bounce(position, velocity)
 
 case object StopPolicy extends BoundaryPolicy:
+  override def apply(position: P2d, velocity: V2d, space: Space): (P2d, V2d) = space.stop(position, velocity)
 
-  override def apply(candidatePosition: P2d, currentVelocity: V2d, space: Space): (P2d, V2d) = space
-    .stop(candidatePosition, currentVelocity)
+case object WrapPolicy extends BoundaryPolicy:
+  override def apply(position: P2d, velocity: V2d, space: Space): (P2d, V2d) = space match
+    case toroidal: Toroidal => (toroidal.wrap(position), velocity)
+    case other              => other.bounce(position, velocity)
 
 object BoundaryPolicy:
-
   val bounce: BoundaryPolicy = BouncePolicy
   val wrap: BoundaryPolicy = WrapPolicy
   val stop: BoundaryPolicy = StopPolicy
--- src/main/scala/domain/CircularSpace.scala
+++ src/main/scala/domain/CircularSpace.scala
@@ -1,7 +1,6 @@
 package domain
 
 final case class CircularSpace(center: P2d, radius: Double) extends Space:
-
   require(radius > 0, "Radius must be positive")
 
   override def contains(position: P2d): Boolean =
@@ -11,46 +10,26 @@ final case class CircularSpace(center: P2d, radius: Double) extends Space:
   override def clamp(position: P2d): P2d =
     val offset = relativeToCenter(position)
     val distance = offset.length
-
     if distance <= radius then position
     else P2d(x = center.x + offset.x / distance * radius, y = center.y + offset.y / distance * radius)
 
   override def bounce(position: P2d, velocity: V2d): (P2d, V2d) =
     val offset = relativeToCenter(position)
     val distance = offset.length
-    val correctedPosition = clamp(position)
-
-    if distance == 0 then (correctedPosition, velocity)
+    if distance == 0 then (clamp(position), velocity)
     else
-      val outwardNormal = V2d(x = offset.x / distance, y = offset.y / distance)
-      val velocityAlongNormal = velocity.x * outwardNormal.x + velocity.y * outwardNormal.y
-      val shouldBounce = distance > radius || (distance >= radius && velocityAlongNormal > 0)
+      val normal = V2d(x = offset.x / distance, y = offset.y / distance)
+      val alongNormal = velocity.x * normal.x + velocity.y * normal.y
+      val shouldBounce = distance >= radius && alongNormal > 0 || distance > radius
       val correctedVelocity =
-        if shouldBounce then
-          V2d(
-            x = velocity.x - 2 * velocityAlongNormal * outwardNormal.x,
-            y = velocity.y - 2 * velocityAlongNormal * outwardNormal.y
-          )
+        if shouldBounce then V2d(velocity.x - 2 * alongNormal * normal.x, velocity.y - 2 * alongNormal * normal.y)
         else velocity
-
-      (correctedPosition, correctedVelocity)
-
-  override def wrap(position: P2d): P2d =
-    throw UnsupportedOperationException("WrapPolicy is not defined for CircularSpace")
+      (clamp(position), correctedVelocity)
 
   override def stop(position: P2d, velocity: V2d): (P2d, V2d) =
     val offset = relativeToCenter(position)
     val distance = offset.length
-    val isOutside = distance > radius
-    val isMovingOutward =
-      if distance == 0 then false
-      else
-        val outwardNormal = V2d(x = offset.x / distance, y = offset.y / distance)
-        velocity.x * outwardNormal.x + velocity.y * outwardNormal.y > 0
-
-    val correctedPosition = clamp(position)
-    val correctedVelocity = if isOutside || isMovingOutward then V2d.zero else velocity
-
-    (correctedPosition, correctedVelocity)
+    val isMovingOutward = distance > 0 && (velocity.x * offset.x / distance + velocity.y * offset.y / distance) > 0
+    (clamp(position), if distance > radius || isMovingOutward then V2d.zero else velocity)
 
   private def relativeToCenter(position: P2d): V2d = V2d(x = position.x - center.x, y = position.y - center.y)
