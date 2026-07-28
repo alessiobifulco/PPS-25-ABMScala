4c972742c8742d9bae3bf3f32ca245c74a2847a6
Samuele Ferri <samuele.ferri3@studio.unibo.it>
2026-07-28 10:40:00 +0200

    feat(gui): add Model with from factory

---
+++ src/main/scala/gui/Model.scala
@@ -0,0 +1,9 @@
+package gui
+
+import engine.{SimulationConfig, SimulationEngine, SimulationState}
+
+case class Model[S](state: SimulationState[S], config: SimulationConfig[S], running: Boolean)
+
+object Model:
+  def from[S](config: SimulationConfig[S]): Model[S] =
+    Model(SimulationEngine.init(config), config, running = false)
