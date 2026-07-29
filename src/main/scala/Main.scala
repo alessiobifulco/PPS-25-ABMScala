57ea1f65663c7fa848f8954713b3895a1b958f64
Alessio Bifulco <alessio.bifulco@studio.unibo.it>
2026-07-29 10:29:29 +0200

    feat(simulations): add simulation two

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,8 @@
 import gui.{MainMenu, SimulationOption, SimulationWindow}
 import simulations.Epidemic
 import simulations.Epidemic.given
+import simulations.OpinionDynamics
+import simulations.OpinionDynamics.given
 
 @main
 def run(): Unit =
@@ -8,5 +10,9 @@ def run(): Unit =
     new SimulationOption:
       def name: String = "Sim 1 - Epidemia"
       def start(): Unit = SimulationWindow.open("Epidemia", Epidemic.config)
+    ,
+    new SimulationOption:
+      def name: String = "Sim 2 - Opinion Dynamics"
+      def start(): Unit = SimulationWindow.open("Opinion Dynamics", OpinionDynamics.config)
   )
   MainMenu.open(options)
