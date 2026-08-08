ff61dd69552a38706fe526ed70f32f7cd3bd0396
Alessio Bifulco <alessio.bifulco@studio.unibo.it>
2026-08-09 00:49:06 +0200

    refactor(domain): refactor domain model for the dsl

--- src/main/scala/domain/Behavior.scala
+++ src/main/scala/domain/Behavior.scala
@@ -4,8 +4,9 @@ trait Behavior[S]:
   def apply(ctx: AgentContext[S]): List[Action[S]]
 
 object Behavior:
+
   def apply[S](f: AgentContext[S] => List[Action[S]]): Behavior[S] = f(_)
 
-  def fromDecision[S](decision: Decision[S]): Behavior[S] = ctx => decision.decide(ctx)
+  def fromDecision[S](decision: Decision[S]): Behavior[S] = Behavior(decision.decide)
 
-  extension [S](b: Behavior[S]) def andThen(other: Behavior[S]): Behavior[S] = ctx => b(ctx) ++ other(ctx)
+  extension [S](b: Behavior[S]) def andThen(other: Behavior[S]): Behavior[S] = Behavior(ctx => b(ctx) ++ other(ctx))
--- src/main/scala/domain/Decision.scala
+++ src/main/scala/domain/Decision.scala
@@ -3,4 +3,7 @@ package domain
 case class Choice[S](condition: AgentContext[S] => Boolean, actions: AgentContext[S] => List[Action[S]])
 
 case class Decision[S](choices: List[Choice[S]]):
-  def decide(ctx: AgentContext[S]): List[Action[S]] = choices.find(_.condition(ctx)).map(_.actions(ctx)).getOrElse(Nil)
+
+  def decide(ctx: AgentContext[S]): List[Action[S]] = choices.find(_.condition(ctx)) match
+    case Some(choice) => choice.actions(ctx)
+    case _            => List.empty
