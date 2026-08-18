ee8d8bfac19a0b7cd45ec0d0a3f1545d171914b7
Alessio Bifulco <alessio.bifulco@studio.unibo.it>
2026-08-18 16:34:36 +0200

    refactor(domain): remove unused behaviour composition

--- src/main/scala/domain/Behavior.scala
+++ src/main/scala/domain/Behavior.scala
@@ -8,5 +8,3 @@ object Behavior:
   def apply[S](f: AgentContext[S] => List[Action[S]]): Behavior[S] = f(_)
 
   def fromDecision[S](decision: Decision[S]): Behavior[S] = Behavior(decision.decide)
-
-  extension [S](b: Behavior[S]) def andThen(other: Behavior[S]): Behavior[S] = Behavior(ctx => b(ctx) ++ other(ctx))
