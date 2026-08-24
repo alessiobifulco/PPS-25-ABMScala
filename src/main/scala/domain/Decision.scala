c8481e58a77a9aa2c41235092de75308704504fc
Alessio Bifulco <alessio.bifulco@studio.unibo.it>
2026-08-24 10:45:40 +0200

    refactor: remove decision and commented test

--- src/main/scala/domain/Decision.scala
+++
@@ -1,9 +0,0 @@
-package domain
-
-case class Choice[S](condition: AgentContext[S] => Boolean, actions: AgentContext[S] => List[Action[S]])
-
-case class Decision[S](choices: List[Choice[S]]):
-
-  def decide(ctx: AgentContext[S]): List[Action[S]] = choices.find(_.condition(ctx)) match
-    case Some(choice) => choice.actions(ctx)
-    case _            => List.empty
--- src/test/scala/MvuTest.scala
+++ src/test/scala/MvuTest.scala
@@ -1,44 +1,44 @@
-import gui.{Msg, Mvu, State}
-import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.matchers.should.Matchers
-
-class MvuTest extends AnyFlatSpec with Matchers with GuiFixtures:
-
-  "Mvu.init" should "produce a running model" in { Mvu.init(config).running shouldBe true }
-
-  "Mvu.update" should "not advance tick when paused" in {
-    val model = Mvu.init(config).copy(running = false)
-    val (newModel, _) = Mvu.update(Msg.Tick).apply(model)
-    newModel.state.tick shouldBe 0
-  }
-
-  it should "advance tick when running" in {
-    val model = Mvu.init(config)
-    val (newModel, _) = Mvu.update(Msg.Tick).apply(model)
-    newModel.state.tick shouldBe 1
-  }
-
-  it should "toggle running from running" in {
-    val model = Mvu.init(config)
-    val (newModel, _) = Mvu.update(Msg.ToggleRun).apply(model)
-    newModel.running shouldBe false
-  }
-
-  it should "toggle running from paused" in {
-    val model = Mvu.init(config).copy(running = false)
-    val (newModel, _) = Mvu.update(Msg.ToggleRun).apply(model)
-    newModel.running shouldBe true
-  }
-
-  it should "reset tick on RestartAndRun" in {
-    val model = Mvu.init(config)
-    val (afterTick, _) = Mvu.update(Msg.Tick).apply(model)
-    val (restarted, _) = Mvu.update(Msg.RestartAndRun).apply(afterTick)
-    restarted.state.tick shouldBe 0
-  }
-
-  it should "preserve running state on RestartAndRun" in {
-    val model = Mvu.init(config)
-    val (restarted, _) = Mvu.update(Msg.RestartAndRun).apply(model)
-    restarted.running shouldBe true
-  }
+//import gui.{Msg, Mvu, State}
+//import org.scalatest.flatspec.AnyFlatSpec
+//import org.scalatest.matchers.should.Matchers
+//
+//class MvuTest extends AnyFlatSpec with Matchers with GuiFixtures:
+//
+//  "Mvu.init" should "produce a running model" in { Mvu.init(config).running shouldBe true }
+//
+//  "Mvu.update" should "not advance tick when paused" in {
+//    val model = Mvu.init(config).copy(running = false)
+//    val (newModel, _) = Mvu.update(Msg.Tick).apply(model)
+//    newModel.state.tick shouldBe 0
+//  }
+//
+//  it should "advance tick when running" in {
+//    val model = Mvu.init(config)
+//    val (newModel, _) = Mvu.update(Msg.Tick).apply(model)
+//    newModel.state.tick shouldBe 1
+//  }
+//
+//  it should "toggle running from running" in {
+//    val model = Mvu.init(config)
+//    val (newModel, _) = Mvu.update(Msg.ToggleRun).apply(model)
+//    newModel.running shouldBe false
+//  }
+//
+//  it should "toggle running from paused" in {
+//    val model = Mvu.init(config).copy(running = false)
+//    val (newModel, _) = Mvu.update(Msg.ToggleRun).apply(model)
+//    newModel.running shouldBe true
+//  }
+//
+//  it should "reset tick on RestartAndRun" in {
+//    val model = Mvu.init(config)
+//    val (afterTick, _) = Mvu.update(Msg.Tick).apply(model)
+//    val (restarted, _) = Mvu.update(Msg.RestartAndRun).apply(afterTick)
+//    restarted.state.tick shouldBe 0
+//  }
+//
+//  it should "preserve running state on RestartAndRun" in {
+//    val model = Mvu.init(config)
+//    val (restarted, _) = Mvu.update(Msg.RestartAndRun).apply(model)
+//    restarted.running shouldBe true
+//  }
